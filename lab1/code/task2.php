5121c608a39d2366a45141882e2923b9e9f241d7
Ivan_Gusev <vanek393956@gmail.com>
2024-03-01 20:21:06 +0200

    solve task2

---
+++ lab1/code/task2.php
@@ -0,0 +1,13 @@
+<?php
+$num = 1;
+echo $num;
+echo "\n";
+$float = 0.32;
+echo $float;
+echo "\n";
+echo 6 + 6;
+echo "\n";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month."\n";
+
