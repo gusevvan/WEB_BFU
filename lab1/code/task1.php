c533424071e156c86e6953eac87a8564ad98c8b5
Ivan_Gusev <vanek393956@gmail.com>
2024-03-01 19:39:59 +0200

    solve task1

---
+++ lab1/code/task1.php
@@ -0,0 +1,11 @@
+<?php
+/* Imagine a lot of code here */ 
+$very_bad_unclear_name = "15 chicken wings";
+
+$order =& $very_bad_unclear_name;
+
+$order .= " and 1 burger";
+  
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
