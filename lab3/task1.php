3f8b8cb961c2358f84c02635ed2a4dc1368b25b9
Ivan_Gusev <vanek393956@gmail.com>
2024-03-23 15:32:34 +0200

    solve task1 b)

--- lab3/task1.php
+++ lab3/task1.php
@@ -7,5 +7,13 @@ $str1 = 'ahb acb aeb aeeb adcb axeb';
 preg_match_all($regexpr1, $str1, $found1);
 print_r($found1);
 
+// task1.b
+$regexpr2 = '/[0-9][0-9]*/';
+$str2 = 'a1b2c3';
 
+function cube($n) {
+    return $n[0] * $n[0] * $n[0];
+}
+
+echo preg_replace_callback($regexpr2, "cube", $str2), "\n";
 
